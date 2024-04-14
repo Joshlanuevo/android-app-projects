1105090dc71d7214e6b2097ef06ecfde7b147c5b
Joshlanuevo <lanuevo68@gmail.com>
2024-04-14 17:29:21 +0800

    generate ramdom numbers

--- lucky-number-app/app/src/main/java/com/example/luckynumberapp/SecondActivity.java
+++ lucky-number-app/app/src/main/java/com/example/luckynumberapp/SecondActivity.java
@@ -2,13 +2,39 @@ package com.example.luckynumberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_text);
+        share_btn = findViewById(R.id.share_btn);
+
+        // Receiving the data from Main Activity
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+
+        // Generating Random Numbers
+        generateRandomNumber();
+    }
+
+    public int generateRandomNumber() {
+        Random random = new Random();
+        int upper_limit = 1000;
+
+        int randomNumberGenerated = random.nextInt(upper_limit);
+        return  randomNumberGenerated;
     }
 }
