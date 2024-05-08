54481d766f97a641b20b1ffbbb700f22eb27c853
Josh Ivan Lanuevo <lanuevo68@gmail.com>
2024-05-08 18:26:41 +0800

    add click function on black button

--- FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.frenchteacherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         redBtn = findViewById(R.id.redBtn);
         purpleBtn = findViewById(R.id.purpleBtn);
         greenBtn = findViewById(R.id.greenBtn);
+        
+        blackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.black);
+                mediaPlayer.start();
+            }
+        });
     }
 }
