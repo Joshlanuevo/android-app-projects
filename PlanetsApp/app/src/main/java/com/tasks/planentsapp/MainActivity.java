90455e213765b0613a1da085d619dad0216b5008
Josh Ivan Lanuevo <lanuevo68@gmail.com>
2024-05-22 14:10:43 +0800

    add planet images

--- PlanetsApp/app/src/main/java/com/tasks/planentsapp/MainActivity.java
+++ PlanetsApp/app/src/main/java/com/tasks/planentsapp/MainActivity.java
@@ -3,11 +3,23 @@ package com.tasks.planentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Planet> planetsArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1 - AdapterView: a ListView
+        listView = findViewById(R.id.listView);
+
+        // 2 - Data Source: ArrayList<Planet>
+        planetsArrayList = new ArrayList<>();
     }
 }
