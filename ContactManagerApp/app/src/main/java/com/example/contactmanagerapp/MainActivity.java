918c23baae0e3684559eb0eff6575cb614cccffc
Joshlanuevo <lanuevo68@gmail.com>
2024-06-12 11:13:13 +0800

    getting data from room db

--- ContactManagerApp/app/src/main/java/com/example/contactmanagerapp/MainActivity.java
+++ ContactManagerApp/app/src/main/java/com/example/contactmanagerapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.contactmanagerapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactmanagerapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // Data Source
+    private ContactDatabase contactDatabase;
+    private ArrayList<Contacts> contacts = new ArrayList<>();
+
+    // Adapter
+    private MyAdapter myAdapter;
+
+    // Binding
+    private ActivityMainBinding mainBinding;
+    private MainActivityClickHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        handler = new MainActivityClickHandler(this);
+
+        mainBinding.setClickHandler(handler);
+
+        // RecyclerView
+        RecyclerView recyclerView = mainBinding.recycleView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        // Adapter
+        myAdapter = new MyAdapter(contacts);
+
+        // Database:
+        contactDatabase = ContactDatabase.getInstance(this);
+
+        // ViewModel:
+        MyViewModel myViewModel = new ViewModelProvider(this)
+                .get(MyViewModel.class);
+
+        // Inserting a new Contact (Just For Testing):
+        Contacts c1 = new Contacts(1, "Ivan", "Ivan@gmail.com");
+        myViewModel.addNewContact(c1);
+
+        // Loading the Data from ROOM DB
+        myViewModel.getAllContacts().observe(this, new Observer<List<Contacts>>() {
+            @Override
+            public void onChanged(List<Contacts> contacts) {
+                for (Contacts c: contacts) {
+                    Log.v("Letsgo", c.getName());
+                }
+            }
+        });
+
+        // Linking the RecyclerView with the Adapter
     }
 }
