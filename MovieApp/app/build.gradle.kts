e1c213f7cc216fd180258f9cf8d9e03f7e050b78
Joshlanuevo <lanuevo68@gmail.com>
2024-06-23 12:13:17 +0800

    add glide

--- MovieApp/app/build.gradle.kts
+++ MovieApp/app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
