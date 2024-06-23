f4a07b9ec16bf6b485225e3c64fd271f5c10ce35
Joshlanuevo <lanuevo68@gmail.com>
2024-06-23 12:32:51 +0800

    use glide with data binding

--- MovieApp/app/src/main/java/com/example/movieapp/model/Movie.java
+++ MovieApp/app/src/main/java/com/example/movieapp/model/Movie.java
@@ -1,10 +1,14 @@
 package com.example.movieapp.model;
 
+import android.widget.ImageView;
+
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
+import androidx.databinding.BindingAdapter;
 
 import java.util.List;
 
+import com.bumptech.glide.Glide;
 import com.example.movieapp.BR;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
@@ -19,6 +23,15 @@ public class Movie extends BaseObservable {
     @SerializedName("poster_path")
     @Expose
     private String posterPath;
+    @BindingAdapter({"posterPath"})
+    public static void loadImg(ImageView imageView, String imageUrl) {
+        // Base Url: "https://image.tmdb.org/t/p/w500/"
+        String imagePath = "https://image.tmdb.org/t/p/w500/"+imageUrl;
+
+        Glide.with(imageView.getContext())
+                .load(imagePath)
+                .into(imageView);
+    }
     @SerializedName("release_date")
     @Expose
     private String releaseDate;
